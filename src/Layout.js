ce30038285804e2695d910eff972675ea7d6ede7
Ao-Tse Lin <shengzhe.lin@icloud.com>
2024-05-01 23:56:20 +0800

    Done the project 8, this is final code

---
+++ src/Layout.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Blogs</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
